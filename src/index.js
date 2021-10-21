51f419e9fade0dc14d455ee5605a09fac86c7ff5
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-21 10:41:41 -0400

    commit tree 33c0432bbe6b6c488eec77f3bfb57052440f1a3f

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // write your code here
+console.log("%c HI", "color: aqua");
+
+const ramenMenu = document.querySelector("#ramen-menu");
+const baseImgUrl = "http://localhost:3000/ramens";
+
+document.addEventListener("DOMContentLoaded", () => {
+  // fetch and translate ramen images into json
+  fetch(baseImgUrl)
+    .then((resp) => resp.json)
+    .then((ramenImages) => getImages(ramenImages));
+});
+
+function items()
